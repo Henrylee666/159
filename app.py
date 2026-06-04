cb0a52075e46fafd4bc39ab856e7e667911a2b56
Henrylee666 <henrylee.960704@gmail.com>
2026-06-04 05:10:15 +0000

    Type what you did.

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 import os
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
@@ -106,6 +109,50 @@ def news():
     return render_template(
         "news.html",
         news_list=news_list
+    )
+@app.route("/quotes")
+def quotes():
+    options = Options()
+    options.binary_location = "/usr/bin/chromium"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.get("https://quotes.toscrape.com/js/")
+
+        quote_elements = driver.find_elements(
+            By.CLASS_NAME,
+            "quote"
+        )
+
+        quote_list = []
+
+        for quote in quote_elements:
+
+            text = quote.find_element(
+                By.CLASS_NAME,
+                "text"
+            ).text
+
+            author = quote.find_element(
+                By.CLASS_NAME,
+                "author"
+            ).text
+
+            quote_list.append({
+                "text": text,
+                "author": author
+            })
+    finally:
+        driver.quit()
+
+        return render_template(
+        "quotes.html",
+        quote_list=quote_list
     )
 
     
