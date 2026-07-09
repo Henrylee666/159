363899c879aee7ec88afe98950ec88fab91ceb6b
Henrylee666 <henrylee.960704@gmail.com>
2026-07-09 03:42:12 +0000

    Use PostgreSQL database

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///app.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
